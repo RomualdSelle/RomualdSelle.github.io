62d95b816d2802847899dc7c3d959d51a600f7f7
Romuald <120457113+RomualdSelle@users.noreply.github.com>
2023-05-09 11:02:52 +0200

    Add files via upload

---
+++ banniere.js
@@ -0,0 +1,67 @@
+function fadeInOutText() {
+    const text1 = document.getElementById("text1");
+    const text2 = document.getElementById("text2");
+    const text3 = document.getElementById("text3");
+
+    setTimeout(() => {
+      text1.classList.add("fadeOut");
+      setTimeout(() => {
+        text1.classList.add("hidden");
+        text2.classList.remove("hidden");
+        text2.classList.add("fadeIn");
+      }, 3000);
+    }, 1000);
+
+    setTimeout(() => {
+      text2.classList.add("fadeOut");
+      setTimeout(() => {
+        text2.classList.add("hidden");
+        text3.classList.remove("hidden");
+        text3.classList.add("fadeIn");
+      }, 3000);
+    }, 5000);
+  }
+
+  window.addEventListener("load", fadeInOutText);
+
+  function openModal(imageSrc) {
+    const modal = document.getElementById("modal");
+    const modalImage = document.getElementById("modalImage");
+
+    modalImage.src = imageSrc;
+    modal.classList.remove("hidden");
+  }
+
+  function closeModal() {
+    const modal = document.getElementById("modal");
+    modal.classList.add("hidden");
+  }
+
+function toggleText(textId, buttonId) {
+  const paragraph = document.getElementById(textId);
+  const button = document.getElementById(buttonId);
+
+  if (paragraph.classList.contains("hidden")) {
+    paragraph.classList.remove("hidden");
+    button.textContent = "Masquer le texte";
+  } else {
+    paragraph.classList.add("hidden");
+    button.textContent = "Afficher le texte";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const backToTopButton = document.getElementById("backToTop");
+
+    window.addEventListener("scroll", () => {
+        if (window.pageYOffset > 100) {
+            backToTopButton.style.display = "flex";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    });
+
+    backToTopButton.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
---
+++ morpion.js
@@ -0,0 +1,188 @@
+var Etatjeu = new Array();
+var iaStarts = false;
+var cells = document.getElementsByClassName("cell");
+var playerScore = 0;
+var iaScore = 0;
+var iaScoreDisplay = document.getElementById("iaScore");
+var playerScoreDisplay = document.getElementById("playerScore");
+
+function remainingMoves(list) {
+    var remaining = new Array();
+    for (i = 0; i < 9; i++) {
+        if (list.indexOf(i) == -1) {
+            remaining.push(i);
+        }
+    }
+    return remaining;
+}
+
+function nextGameStates(list) {
+    var nextStates = new Array();
+    remaining = remainingMoves(list);
+    for (i = 0; i < remaining.length; i++) {
+        nextStates.push([remaining[i]]);
+    }
+    for (i = 0; i < remaining.length; i++) {
+        nextStates[i] = list.concat(nextStates[i]);
+    }
+    return nextStates;
+}
+
+function hasWon(list) {
+    var board = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0);
+    var player = (list.length + 1) % 2;
+    for (i = player; i < list.length; i = i + 2) {
+        board[list[i]] = 1;
+    }
+    if (
+        (board[0] == 1 && board[1] == 1 && board[2] == 1) ||
+        (board[3] == 1 && board[4] == 1 && board[5] == 1) ||
+        (board[6] == 1 && board[7] == 1 && board[8] == 1) ||
+        (board[0] == 1 && board[3] == 1 && board[6] == 1) ||
+        (board[1] == 1 && board[4] == 1 && board[7] == 1) ||
+        (board[2] == 1 && board[5] == 1 && board[8] == 1) ||
+        (board[0] == 1 && board[4] == 1 && board[8] == 1) ||
+        (board[2] == 1 && board[4] == 1 && board[6] == 1)
+    ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function bestMove(list, depth) {
+    if (depth == 0) {
+        threshold = remainingMoves(list).length - 1;
+    }
+    var maxStrength = Math.pow(-1, depth + 1);
+    var Strength;
+    var move = 9;
+    var i;
+    for (i = 0; i < 9 - list.length; i++) {
+        if (depth % 2 == 0) {
+            Strength = hasWon(nextGameStates(list)[i]);
+            if (Strength == 1) {
+                return [Strength, remainingMoves(list)[i]];
+            } else {
+                if (depth < threshold) {
+                    Strength = bestMove(nextGameStates(list)[i], depth + 1)[0];
+                    if (Strength > maxStrength) {
+                        maxStrength = Strength;
+                        move = remainingMoves(list)[i];
+                    }
+                } else {
+                    return [hasWon(list), remainingMoves(list)[0]];
+                 }
+            }
+        }
+        if (depth % 2 == 1) {
+            Strength = -1 * hasWon(nextGameStates(list)[i]);
+            if (Strength == -1) {
+                return [Strength, remainingMoves(list)[i]];
+            } else {
+                if (depth < threshold) {
+                    Strength = bestMove(nextGameStates(list)[i], depth + 1)[0];
+                    if (Strength < maxStrength) {
+                        maxStrength = Strength;
+                        move = remainingMoves(list)[i];
+                    }
+                } else {
+                    return [hasWon(list), remainingMoves(list)[0]];
+                }
+            }
+        }
+    }
+    return [maxStrength, move];
+}
+
+function matchNul(list) {
+    return list.length === 9 && hasWon(list) === 0;
+}
+
+function aGagneJoueur(list, joueur) {
+    var board = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0);
+    for (i = joueur; i < list.length; i += 2) {
+        board[list[i]] = 1;
+    }
+
+    var conditionsDeVictoire = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    var winningIndices = null;
+    conditionsDeVictoire.some(function (condition) {
+        if (condition.every(function (index) { return board[index] === 1; })) {
+            winningIndices = condition;
+            return true;
+        }
+        return false;
+    });
+
+    return winningIndices;
+}
+
+
+function checkGameOver() {
+    var winningIndices = aGagneJoueur(Etatjeu, 0) || aGagneJoueur(Etatjeu, 1);
+
+    if (winningIndices) {
+        winningIndices.forEach(function (index) {
+            cells[index].classList.add("blink");
+        });
+        iaScore++;
+        iaScoreDisplay.textContent = iaScore;
+        gameOver();
+    } else if (matchNul(Etatjeu)) {
+        gameOver();
+    }
+}
+
+
+function play(cell) {
+    var index = Array.from(cells).indexOf(cell);
+    if (Etatjeu.indexOf(index) === -1) {
+        Etatjeu.push(index);
+        cell.textContent = "X";
+        checkGameOver();
+        if (Etatjeu.length < 9) {
+            var iaMove = bestMove(Etatjeu, 0)[1];
+            Etatjeu.push(iaMove);
+            cells[iaMove].textContent = "O";
+            checkGameOver();
+        }
+    }
+}
+
+
+
+function gameOver() {
+    for (let cell of cells) {
+        cell.removeEventListener("click", clickHandler);
+    }
+    setTimeout(resetGame, 1000);
+}
+
+function clickHandler(event) {
+    play(event.target);
+}
+
+function resetGame() {
+    for (let cell of cells) {
+        cell.textContent = "";
+        cell.classList.remove("blink"); // Ajoutez cette ligne
+        cell.addEventListener("click", clickHandler);
+    }
+    Etatjeu = [];
+    iaStarts = !iaStarts;
+    if (iaStarts) {
+        var iaMove = bestMove(Etatjeu, 0)[1];
+        Etatjeu.push(iaMove);
+        cells[iaMove].textContent = "O";
+    }
+}
+
+for (let cell of cells) {
+    cell.addEventListener("click", clickHandler);
+}
